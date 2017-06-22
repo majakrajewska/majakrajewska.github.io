249422272cd9c42d9fa51d3c24d17a1ba3686086
majakrajewska <majamkrajewska@gmail.com>
2017-06-22 18:25:18 +0200

    Add files via upload

---
+++ skrypty.js
@@ -0,0 +1,39 @@
+$(document).on('mouseenter', '.parkof', function () {
+    $('body').addClass('parkof-backgrounds');
+});
+$(document).on('mouseleave', '.parkof', function () {
+    $('body').removeClass('parkof-backgrounds');
+});
+
+
+$(document).on('mouseenter', '.cityview', function () {
+    $('body').addClass('cityview-backgrounds');
+});
+$(document).on('mouseleave', '.cityview', function () {
+    $('body').removeClass('cityview-backgrounds');
+});
+
+
+$(document).on('mouseenter', '.thecolorstudy', function () {
+    $('body').addClass('thecolorstudy-backgrounds');
+});
+$(document).on('mouseleave', '.thecolorstudy', function () {
+    $('body').removeClass('thecolorstudy-backgrounds');
+});
+
+
+$(document).on('mouseenter', '.calendar', function () {
+    $('body').addClass('calendar-backgrounds');
+});
+$(document).on('mouseleave', '.calendar', function () {
+    $('body').removeClass('calendar-backgrounds');
+});
+
+$(document).on('mouseenter', '.projectline', function () {
+    $('body').addClass('projectline-backgrounds');
+});
+$(document).on('mouseleave', '.projectline', function () {
+    $('body').removeClass('projectline-backgrounds');
+});
+
+
